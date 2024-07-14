3a5545e28afbb0026935fa4618f691f81f645699
sadia-hossain2001 <sadiahossian3008@gmail.com>
2024-07-14 20:31:51 +0600

    jog

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,10 +5,14 @@ import { Route, Routes } from 'react-router-dom';
 import Orders from './pages/Orders/Orders';
 import List from './pages/List/List';
 import Add from './pages/Add/Add';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const App = () => {
   return (
     <div>
+      <ToastContainer/>
       <Navbar/>
       <hr/>
       <div className="app-content">
--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
-import './List.css'
+import React, { useState,useEffect } from "react";
+import "./List.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 const List = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const url = "http://localhost:4000";
+  const [list, setList] = useState([]);
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    console.log(response.data);
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+  useEffect(()=>{
+    fetchList();
+  },[])
 
-export default List
+  return (
+    <div className="list add flex-col">
+      <p>All Deserts List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item,index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/` +item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>{item.price}tk</p>
+              <p className="cursor">X</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
