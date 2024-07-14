4a09963c1e4e80015996179dee6ce6d946da848f
sadia-hossain2001 <sadiahossian3008@gmail.com>
2024-07-15 01:54:31 +0600

    have

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -2,18 +2,27 @@ import React, { useState,useEffect } from "react";
 import "./List.css";
 import axios from "axios";
 import { toast } from "react-toastify";
-const List = () => {
-  const url = "http://localhost:4000";
+
+const List = ({url}) => {
   const [list, setList] = useState([]);
   const fetchList = async () => {
     const response = await axios.get(`${url}/api/food/list`);
-    console.log(response.data);
     if (response.data.success) {
       setList(response.data.data);
     } else {
       toast.error("Error");
     }
   };
+  const removeFood=async(foodId)=>{
+const response= await axios.post(`${url}/api/food/remove`,{id:foodId});
+await fetchList();
+if(response.data.success){
+toast.success(response.data.message)
+}
+else{
+  toast.error("Error");
+}
+  }
   useEffect(()=>{
     fetchList();
   },[])
@@ -36,7 +45,7 @@ const List = () => {
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>{item.price}tk</p>
-              <p className="cursor">X</p>
+              <p onClick={()=>removeFood(item._id)} className="cursor">X</p>
             </div>
           );
         })}
